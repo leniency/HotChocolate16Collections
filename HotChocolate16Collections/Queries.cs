70d515491c68931b89923f9fda51957a5af10022
Levi Baker <levi.baker@esd112.org>
2026-05-28 16:13:25 -0700

    minor cleanup

--- HotChocolate16Collections/Queries.cs
+++ HotChocolate16Collections/Queries.cs
@@ -1,7 +1,4 @@
-﻿using HotChocolate.Configuration;
-using HotChocolate.Types.Descriptors;
-using HotChocolate.Types.Descriptors.Configurations;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace HotChocolate16Collections;
 
@@ -40,85 +37,4 @@ public class InstanceQuery
     // Works as expected.
 
     public string Hello2([StringLength(50)] string name) => $"Hello, {name}!";
-}
-
-
-/// <summary>
-/// Apply DataAnnotations validations as schema directives.
-/// </summary>
-class ValidationDirectiveInterceptor : TypeInterceptor
-{
-
-    /// <summary>
-    /// Apply directives to schema types.
-    /// </summary>
-    /// <param name="discoveryContext"></param>
-    /// <param name="configuration"></param>
-    public override void OnBeforeRegisterDependencies(ITypeDiscoveryContext discoveryContext, TypeSystemConfiguration configuration)
-    {
-        // Object types
-        if (configuration is ObjectTypeConfiguration obj)
-        {
-            // Apply to fields
-            foreach (var field in obj.Fields)
-            {
-                ApplyAttributeDirectives(field, f => f?.Member?.GetCustomAttributes(true), discoveryContext);
-
-                // Apply to arguments
-                foreach (var argument in field.Arguments)
-                {
-                    if (argument is not { Parameter: { } parameter })
-                    {
-                        continue;
-                    }
-
-                    ApplyAttributeDirectives(argument, f => parameter.GetCustomAttributes(true), discoveryContext);
-                }
-            }
-        }
-
-        // Input object types
-        if (configuration is InputObjectTypeConfiguration inputObjectTypeDefinition)
-        {
-            foreach (var field in inputObjectTypeDefinition.Fields)
-            {
-                if (field.Property != null)
-                {
-                    ApplyAttributeDirectives(field, f => f?.Property?.GetCustomAttributes(true), discoveryContext);
-                }
-            }
-        }
-    }
-
-    void ApplyAttributeDirectives<T>(T field, Func<T, object[]?> attrs, ITypeDiscoveryContext discoveryContext)
-        where T : FieldConfiguration
-    {
-        var attributes = attrs(field);
-        if (attributes == null || attributes.Length == 0)
-        {
-            return;
-        }
-
-        ApplyAttributes(field, attributes, discoveryContext);
-    }
-
-    static void ApplyAttributes<T>(T field, IEnumerable<object> attributes, ITypeDiscoveryContext discoveryContext)
-        where T : FieldConfiguration
-    {
-        foreach (var attribute in attributes)
-        {
-            if (attribute is ValidationAttribute)
-            {
-                // ... other stuff around the specific validation attribute...
-
-                // Add to the field.
-                var directive = new ValidationDirective();
-                var validationTypeRef = TypeReference.CreateDirective(discoveryContext.TypeInspector.GetType(directive.GetType()));
-
-                field.Directives.Add(new DirectiveConfiguration(
-                    directive,
-                    validationTypeRef));
-            }
-        }
-    }
 }
---
+++ HotChocolate16Collections/ValidationDirectiveInterceptor.cs
@@ -0,0 +1,85 @@
+﻿using HotChocolate.Configuration;
+using HotChocolate.Types.Descriptors;
+using HotChocolate.Types.Descriptors.Configurations;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotChocolate16Collections;
+
+/// <summary>
+/// Apply DataAnnotations validations as schema directives.
+/// </summary>
+class ValidationDirectiveInterceptor : TypeInterceptor
+{
+    /// <summary>
+    /// Apply directives to schema types.
+    /// </summary>
+    /// <param name="discoveryContext"></param>
+    /// <param name="configuration"></param>
+    public override void OnBeforeRegisterDependencies(ITypeDiscoveryContext discoveryContext, TypeSystemConfiguration configuration)
+    {
+        // Object types
+        if (configuration is ObjectTypeConfiguration obj)
+        {
+            // Apply to fields
+            foreach (var field in obj.Fields)
+            {
+                ApplyAttributeDirectives(field, f => f?.Member?.GetCustomAttributes(true), discoveryContext);
+
+                // Apply to arguments
+                foreach (var argument in field.Arguments)
+                {
+                    if (argument is not { Parameter: { } parameter })
+                    {
+                        continue;
+                    }
+
+                    ApplyAttributeDirectives(argument, f => parameter.GetCustomAttributes(true), discoveryContext);
+                }
+            }
+        }
+
+        // Input object types
+        if (configuration is InputObjectTypeConfiguration inputObjectTypeDefinition)
+        {
+            foreach (var field in inputObjectTypeDefinition.Fields)
+            {
+                if (field.Property != null)
+                {
+                    ApplyAttributeDirectives(field, f => f?.Property?.GetCustomAttributes(true), discoveryContext);
+                }
+            }
+        }
+    }
+
+    void ApplyAttributeDirectives<T>(T field, Func<T, object[]?> attrs, ITypeDiscoveryContext discoveryContext)
+        where T : FieldConfiguration
+    {
+        var attributes = attrs(field);
+        if (attributes == null || attributes.Length == 0)
+        {
+            return;
+        }
+
+        ApplyAttributes(field, attributes, discoveryContext);
+    }
+
+    static void ApplyAttributes<T>(T field, IEnumerable<object> attributes, ITypeDiscoveryContext discoveryContext)
+        where T : FieldConfiguration
+    {
+        foreach (var attribute in attributes)
+        {
+            if (attribute is ValidationAttribute)
+            {
+                // ... other stuff around the specific validation attribute...
+
+                // Add to the field.
+                var directive = new ValidationDirective();
+                var validationTypeRef = TypeReference.CreateDirective(discoveryContext.TypeInspector.GetType(directive.GetType()));
+
+                field.Directives.Add(new DirectiveConfiguration(
+                    directive,
+                    validationTypeRef));
+            }
+        }
+    }
+}
